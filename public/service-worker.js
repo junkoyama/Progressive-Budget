057dc3524de63f6a94985f4809dcb873fe863739
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-16 11:48:38 -0400

    Commented out stackpath to test out error

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,13 +4,13 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/db.js",
-  "/index.js",
   "/styles.css",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+  // 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  // "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0"
 
 ];
 
